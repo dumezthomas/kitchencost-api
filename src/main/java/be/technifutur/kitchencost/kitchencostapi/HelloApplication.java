130dbb7565f8f85600b2a2b67b5d81b7b50cdf76
Thomas Dumez <dumezthomas@gmail.com>
2026-05-04 10:20:22 +0200

    Initial commit

---
+++ src/main/java/be/technifutur/kitchencost/kitchencostapi/HelloApplication.java
@@ -0,0 +1,9 @@
+package be.technifutur.kitchencost.kitchencostapi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
